d9ae2a306aab074de8f411218c7cfc59b687ab00
natalia <spam.nkhrush@gmail.com>
2023-03-02 17:19:52 +0300

    add dotnet training 001 task 2

---
+++ Dotnet_Training001_task2/Program.cs
@@ -0,0 +1,10 @@
+﻿// 2. Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+//     4 -> 16 
+//     -3 -> 9 
+//     -7 -> 49
+
+Console.WriteLine("Введите число ");
+int N = int.Parse(Console.ReadLine());
+int result;
+result = N * N;
+Console.WriteLine (result);
