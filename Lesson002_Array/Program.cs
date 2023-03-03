985d7f1241af47d9e3cbb847fc73b5367e21b1be
natalia <spam.nkhrush@gmail.com>
2023-03-03 13:27:10 +0300

    added lesson abour Array

---
+++ Lesson002_Array/Program.cs
@@ -0,0 +1,10 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int [] array = {11, 21, 31, 41, 51, 61, 71, 81, 91};
+array[0] = 12; // таким образом можно обратиться к массиву и записать в него значение
+Console.WriteLine(array[0]); // таким образом можно обратиться к массиву и получить значение соответствующего элемента по указанному индексу
