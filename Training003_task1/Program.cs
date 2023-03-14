cd112bb1558f346d396cc9d9196b50085e658551
natalia <spam.nkhrush@gmail.com>
2023-03-14 13:59:54 +0300

    add training 003 task 1

---
+++ Training003_task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Покахать четные числа от 1 до N
+
+
+string EvenNumbers(int N)
+{
+    string output = "";
+    int index = 2;
+    while (index <= N)
+    {
+        output = output + index + " ";
+        index = index + 2;
+    }
+    return output;
+}
+
+int GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int N = GetNumber("Введите число ");
+string result = EvenNumbers(N);
+Console.WriteLine(result);
+File.WriteAllText("file.txt", result);
