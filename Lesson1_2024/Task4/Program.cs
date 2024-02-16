61c11437237babd589c66057662d295425cb8fda
natalia <spam.nkhrush@gmail.com>
2024-02-16 15:47:17 +0300

    add lesson1:Task4

---
+++ Lesson1_2024/Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿int a = 756;
+int b = 57;
+int c = 9;
+int d = 4;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+if (d > max)
+{
+    max = d;
+}
+
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
