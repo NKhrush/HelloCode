3297f2243a0ab6a4695692e1f8ae24b34560e38b
natalia <spam.nkhrush@gmail.com>
2024-02-23 20:53:10 +0300

    added corrections to LESSONS

--- LESSONS/Program.cs
+++ LESSONS/Program.cs
@@ -1,21 +1,22 @@
 ﻿// ДВУМЕРНЫЕ МАССИВЫ:
 
-// string[,] table = new string[2, 5];
-// // String.Empty
-// // table[0,0] table[0,1] table[0,2] table [0,3] table[0,4]
-// // table[1,0] table[1,1] table[1,2] table [1,3] table[1,4]
+// string[,] table = new string[3, 4];
+// //String.Empty;
+// //table[0,0] table[0,1] table[0,2] table [0,3] table[0,4];
+// //table[1,0] table[1,1] table[1,2] table [1,3] table[1,4];
 
-// table[1, 2] = "слово";
+// //table[1, 2] = "слово";
 
-// for (int rows = 0; rows < 2; rows++)
+// for (int i = 0; i < table.GetLength(0); i++)
 // {
-//     for (int columns = 0; columns < 5; columns++)
+//     for (int j = 0; j < table.GetLength(1); j++)
 //     {
-//         System.Console.WriteLine($"-{table[rows,columns]}-");
+//         System.Console.Write($"0{table[i,j]} ");
 //     }
+// Console.WriteLine();
 // }
 
-using static ex001;
+ using static ex001;
 
 // int[,] matrix = new int [3, 4];
 // PrintArray(matrix);
@@ -97,13 +98,13 @@ using static ex001;
 // f(2) = 1
 // f(n) = f(n-1) + f(n-2)
 
-double Fibonacci(int n)
-{
-    if (n == 1 || n == 2) return 1;
-    else return Fibonacci(n-1) + Fibonacci(n-2);
-}
+// double Fibonacci(int n)
+// {
+//     if (n == 1 || n == 2) return 1;
+//     else return Fibonacci(n-1) + Fibonacci(n-2);
+// }
 
-for (int i = 1; i < 50; i++)
-{
-    Console.WriteLine($"{i}) = {Fibonacci(i)}");
-}
+// for (int i = 1; i < 50; i++)
+// {
+//     Console.WriteLine($"{i}) = {Fibonacci(i)}");
+// }
