265b3b23824260566071ae599bb977f1176a2f59
natalia <spam.nkhrush@gmail.com>
2024-02-25 17:53:07 +0300

    added Lessn2:Task2

---
+++ Lesson2_2024/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Массивы
+
+
+int[] array = new int[9];
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string isFind = "Нет";
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 17);
+    System.Console.Write(array[i] + " ");
+
+    if(num == array[i])
+    {
+        isFind = "Да";
+    }
+}
+Console.WriteLine(isFind);
