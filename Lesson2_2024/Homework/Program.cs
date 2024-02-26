27d865a63a7e31b92843f26b89516cf1f68cafcc
natalia <spam.nkhrush@gmail.com>
2024-02-26 17:52:26 +0300

    Added Lesson2:Homework

---
+++ Lesson2_2024/Homework/Program.cs
@@ -0,0 +1,26 @@
+﻿// Количество элементов массива на отрезке
+
+// Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [10,90].
+
+// Начальные условия:
+
+// int[] array = {1, 5, 10, 20, 30, 40, 99, 4, 90, 3}; // Создание массива
+// Выводится: 5
+
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 20);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 10 && array[i] < 90)
+    {
+        count = count + 1;
+    }
+}
+Console.WriteLine(count);
