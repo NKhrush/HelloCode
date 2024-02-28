16651085ca1752b3fb703b624fdfc54fde897d08
natalia <spam.nkhrush@gmail.com>
2024-02-28 20:57:57 +0300

    Added Lesson3:Task4-5

--- Lesson3_2024/Task4/Program.cs
+++ Lesson3_2024/Task4/Program.cs
@@ -7,6 +7,8 @@ int[] arr_int = new int[size];
 int i = 0;
 while(i < size)
 {
+    Console.WriteLine("Введите целое число");
+    string input = Console.ReadLine();
     arr_int[i] = rnd.Next(1, 11);
     i = i + 1;
 }
---
+++ Lesson3_2024/Task5/Program.cs
@@ -0,0 +1,6 @@
+﻿// Ввод значений с консоли
+ 
+ Console.WriteLine("Введите целое число");
+ string input = Console.ReadLine();
+
+ int value = Convert.ToInt32(input);
