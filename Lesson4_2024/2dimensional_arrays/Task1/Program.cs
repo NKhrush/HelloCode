1628214ea2f163802ee01ac8b45b66ae684e2e29
natalia <spam.nkhrush@gmail.com>
2024-03-12 15:17:07 +0300

    Added Lesson4:Tasks1-2

---
+++ Lesson4_2024/2dimensional_arrays/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+int[,] CreateMatrix(int rowCount, int columnsCount)
+{
+    int[,] matrix = new int[rowCount, columnsCount];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) //запрашиваем колво строк в массиве
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) //запрашиваем колва столбцов (1е измерение)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) //запрашиваем колво строк в массиве
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) //запрашиваем колва столбцов (1е измерение)
+        {
+            Console.Write($"{matrix[i, j]} "); //интерполяция строк
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] matrix = CreateMatrix(4,5);
+ShowMatrix(matrix);
