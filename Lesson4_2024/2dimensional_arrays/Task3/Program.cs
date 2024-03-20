42053679960d2d83876256ea6ccb7a2f0be56ecf
natalia <spam.nkhrush@gmail.com>
2024-03-20 12:39:24 +0300

    Added new tasks to Lesson4_2024

---
+++ Lesson4_2024/2dimensional_arrays/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Считать с консоли строку, состоящую из цифр и латинских букв.
+// Сформировать новую строку, состаящую из букв исходной строки. 
+
+// 123passw0rd
+
+string GetLettersFromString (string s)
+{
+    string letters  = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+Console.WriteLine(result);
---
+++ Lesson4_2024/2dimensional_arrays/Task5/Program.cs
@@ -0,0 +1,53 @@
+﻿// находящихся на главной диагонали (с индексами (0,0); (1;1) и
+// т.д.
+// Пример
+// 2 3 4 3
+// 4 3 4 1 => 2 + 3 + 5 = 10
+// 2 9 5 4
+
+int[,] array = new int[4,5];
+
+void FillingArray()
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+array[i,j]= new Random().Next(1,10);
+}
+}
+}
+
+void PrintArray()
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+System.Console.Write(array[i,j]+ " ");
+}
+System.Console.WriteLine();
+}
+}
+
+FillingArray();
+PrintArray();
+System.Console.WriteLine(GetCountNumberInArray());
+
+
+int GetCountNumberInArray()
+{
+int sum =0;
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+if(i==j)
+{
+sum= sum+ array[i,j];
+}
+}
+}
+return sum;
+}
