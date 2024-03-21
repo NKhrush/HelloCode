9b159e4268405ad8200c558916cf5519c181c87e
natalia <spam.nkhrush@gmail.com>
2024-03-21 18:00:41 +0300

    Added Lesson4:HWTasks

---
+++ Lesson4_2024/2dimensional_arrays/HomeworkTask3/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Начальные условия:
+
+// // Начальные условия
+// int[,] numbers = new int[,] {
+//     {1, 2, 3},
+//     {1, 1, 0},
+//     {7, 8, 2},
+//     {9, 10, 11}
+// };
+// Выводится: 1
+
+
+/// Вычисление сумм по строкам (на выходе массив с суммами строк)
+    public static int[] SumRows(int[,] array)
+    {
+        int[] sum = new int[array.GetLength(0)];
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                sum[i] += array[i, j];
+            }
+        }
+        return sum;
+    }
+
+    // Получение индекса минимального элемента в одномерном массиве
+    public static int MinIndex(int[] array)
+    {
+        int minI = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[minI] > array[i])
+            {
+                minI = i;
+            }
+        }
+        return minI;
+    }
+    public static void PrintResult(int[,] numbers)
+    {   
+        int[] sums = SumRows(numbers);
+        int minIndex = MinIndex(sums);
+        Console.Write(minIndex);
+    }
