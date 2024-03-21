9b30a7d04b4adef89ac266157b5f24396a3c5814
natalia <spam.nkhrush@gmail.com>
2024-03-21 18:21:16 +0300

    Added Lesson4:arrays&strings:Task1

---
+++ Lesson4_2024/arrays&strings/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте массив символов (тип char []). Создайте строку из
+// символов этого массива.
+
+char[] array = new char[]{'t', '4', '#', 'y', '9'};
+
+string str ="";
+
+// for (int i = 0; i < array.Length; i++)
+// {
+// str = str + array[i];
+// }
+// System.Console.WriteLine(str);
+
+foreach (char item in array)
+{
+str = str + item;
+}
+
+System.Console.WriteLine(str);
