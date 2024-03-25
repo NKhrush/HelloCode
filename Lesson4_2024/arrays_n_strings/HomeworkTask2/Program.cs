7c820d7e1564a6e18285ec4b28dd870353150fda
natalia <spam.nkhrush@gmail.com>
2024-03-25 18:23:01 +0300

    Added Lesson4:strings and arrays:Homework

---
+++ Lesson4_2024/arrays_n_strings/HomeworkTask2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+Console.WriteLine("Введите строчку: ");
+string userInput = Console.ReadLine();
+
+System.Text.StringBuilder str = new System.Text.StringBuilder(userInput);
+        
+for (int j = 0; j < str.Length; j++)
+{
+    if (System.Char.IsUpper(str[j]) == true)
+        str[j] = System.Char.ToLower(str[j]);
+}
+Console.Write (str);
