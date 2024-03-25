41c73fd41c3ab045ed1c9eb31c86867014c463f9
natalia <spam.nkhrush@gmail.com>
2024-03-25 16:12:57 +0300

    Added Lesson4:arrays and string:Task3

---
+++ Lesson4_2024/arrays_n_strings/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// На основе символов строки (тип string) сформировать массив
+// символов (тип char[]). Вывести массив на экран
+
+string str = Console.ReadLine();
+char[] array = new char[str.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = str[i];
+System.Console.Write(array[i]);
+}
