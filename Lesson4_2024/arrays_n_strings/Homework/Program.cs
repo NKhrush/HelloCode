b527b058859a80ea78523fabff56697b7db3d0ff
natalia <spam.nkhrush@gmail.com>
2024-03-25 15:47:17 +0300

    Added Lesson4:arrays and strings:HomeworkTask1

---
+++ Lesson4_2024/arrays_n_strings/Homework/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). 
+// Создать строку из символов этого массива.
+
+char[,] array = new char[,]{
+{'t', '4', '#', 'y', '9'},
+{'t', '4', '#', 'y', '9'},
+{'t', '4', '#', 'y', '9'},
+};
+
+string str = "";
+
+        foreach (char item in array)
+        {
+            str = str + item;
+        }
+
+System.Console.WriteLine(str);
