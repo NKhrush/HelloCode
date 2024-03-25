f671dc4fe7177570e7ea1254484c421dc09baf66
natalia <spam.nkhrush@gmail.com>
2024-03-25 16:24:39 +0300

    Added Lessone4:strings and arrays:Task4

---
+++ Lesson4_2024/arrays_n_strings/Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Считать строку с консоли, состоящую из латинских
+// букв в нижнем регистре. Выяснить, сколько среди
+// введённых букв гласных.
+// Пример
+// “hello” => 2
+// “world” => 1
+
+System.Console.WriteLine("Введите срочку");
+string userInput = Console.ReadLine();
+
+string vowels = "aeiuo";
+int count= 0;
+
+foreach (var item in userInput)
+{
+if(vowels.Contains(item))
+{
+count++;
+}
+}
+System.Console.WriteLine(count);
