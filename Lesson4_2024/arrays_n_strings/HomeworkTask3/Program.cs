129d710b958706dc7d8e0d68efa0f43dc7950dac
natalia <spam.nkhrush@gmail.com>
2024-03-26 16:03:19 +0300

    Added Lesson4:arrays and strings:HomeworkTask3

---
+++ Lesson4_2024/arrays_n_strings/HomeworkTask3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+int GetNumber (string text)
+    {
+        System.Console.WriteLine(text);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+ string SearchArgs(int number)
+    {
+        string result = String.Empty;
+        int arg1 = number / 10000;
+        int arg2 = number % 10;
+        int arg3 = number / 1000 % 10;
+        int arg4 = number / 100 % 10;
+        if (arg1 == arg2 || arg3 == arg4)
+            {
+               result = result + "Это палиндром!";
+               return result;
+            }
+         else 
+            {
+                result = result + "Это не палиндром :(";
+                return result;
+            }
+    }
+
+
+int number = GetNumber("Введите пятизначное число ");
+Console.WriteLine (SearchArgs(number));
