fa7808a561058cccb57dc47aadf273102301ef96
natalia <spam.nkhrush@gmail.com>
2024-04-02 20:07:50 +0300

    Added Lesson5:HomeworkTask1

--- Lesson5_2024/HomeworkTask1/Program.cs
+++ Lesson5_2024/HomeworkTask1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+
+
+int GetNumber(string text)
+ {
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+ }
+
+void PrintNumbers (int M, int N)
+{
+    if (M > N) return;
+    Console.Write (M + " ");
+    PrintNumbers (M + 1, N);
+    
+}
+
+int numberOne = GetNumber("Введите значение M: ");
+int numberTwo = GetNumber("Введите значение N: ");
+PrintNumbers(numberOne, numberTwo);
