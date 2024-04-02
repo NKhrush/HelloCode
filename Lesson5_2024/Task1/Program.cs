7ecf7d97746d1fd021b5f8db4960763fc67c95f4
natalia <spam.nkhrush@gmail.com>
2024-04-02 20:11:41 +0300

    Added solution to Recursion:Task2

--- Lesson5_2024/Task1/Program.cs
+++ Lesson5_2024/Task1/Program.cs
@@ -26,15 +26,15 @@
 
 int ReadInt(string msg)
 {
-System.Console.Write(msg);
-return Convert.ToInt32(Console.ReadLine());
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
 }
 
 void PrintNumbers(int N) // 7 -> 6 -> 5 -> 4 -> 3 -> 2 -> 1
 {
-if(N < 1) return; // Условие остановки рекурсии
-PrintNumbers(N - 1);
-System.Console.Write(N + " ");
+    if (N < 1) return; // Условие остановки рекурсии
+    PrintNumbers(N - 1);
+    System.Console.Write(N + " ");
 }
 
 // ----------------- ОСНОВНОЙ КОД ---------------
