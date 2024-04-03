be6f28031f4cbfe75f8d431585e5c45573ff42be
natalia <spam.nkhrush@gmail.com>
2024-04-03 15:02:42 +0300

    Added Lesson5:HomeworkTask3

---
+++ Lesson5_2024/HomeworkTask3/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 3: Задайте произвольный массив. Выведите его элементы, 
+// начиная с конца. Использовать рекурсию, не использовать циклы.
+
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+
+void FillRandomArray(int[] collection, int index = 0)
+{
+    
+    if (index == collection.Length) return;
+    else 
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+        FillRandomArray(collection, index);
+    }
+}
+
+void PrintArray(int[] array, int index = 0)
+{
+
+    if (index == array.Length) return;
+    else
+    {
+        Console.Write(array[index++] + " ");
+        PrintArray(array, index);
+    }
+}
+
+void PrintArrayBackwards(int[] array, int index)
+{
+
+    if (index == 0) return;
+    else
+    {
+        Console.Write(array[index-1] + " ");
+        index = index - 1;
+        PrintArrayBackwards(array, index);
+    }
+}
+
+int[] array = CreateArray(5);
+FillRandomArray(array);
+PrintArray(array);
+Console.WriteLine();
+PrintArrayBackwards(array, array.Length);
