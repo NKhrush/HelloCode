fa6e42203d6c2dba05dd7af730a1d83258ce7fcd
natalia <spam.nkhrush@gmail.com>
2024-04-13 14:59:24 +0300

    Added solution to MainBlock:FinalTest

--- MainBlock_FinalTest/Program.cs
+++ MainBlock_FinalTest/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] CreateArray(int size)
+    {
+        return new string [size];
+    }
+
+void FillByUser(string[] array)
+    {
+        int size = array.Length;
+
+        int index = 0;
+
+        while (index < size)
+        {
+            array[index] = GetSymbol("Введите " + index + " элемент: ");
+            index++;
+        }
+    }
+
+string GetSymbol (string text)
+    {
+        System.Console.WriteLine(text);
+        return Console.ReadLine();
+    }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+Console.WriteLine ("Введите размер массива: ");
+int str =  Convert.ToInt32 (Console.ReadLine());
+string [] array = CreateArray (str);
+FillByUser(array);
+Console.WriteLine ("Первоначальный массив: ");
+PrintArray(array);
